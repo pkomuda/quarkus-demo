bddc8490450e966bdfe9a30c476ae7af186a381e
Przemysław Komuda <p.komuda1@gmail.com>
2024-10-23 20:18:00 +0200

    Add service

---
+++ src/main/java/com/github/pkomuda/BookService.java
@@ -0,0 +1,32 @@
+package com.github.pkomuda;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Transactional
+@ApplicationScoped
+@RequiredArgsConstructor
+public class BookService {
+
+    private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
+
+    public void addBook(BookDto bookDto) {
+        bookRepository.persistAndFlush(bookMapper.toEntity(bookDto));
+    }
+
+    public BookDto getBook(String isbn) {
+        return bookRepository.findByIsbn(isbn)
+                .map(bookMapper::toDto)
+                .orElseThrow(() -> new RuntimeException("Book not found"));
+    }
+
+    public List<BookDto> getAllBooks() {
+        return bookRepository.findAll().stream()
+                .map(bookMapper::toDto)
+                .toList();
+    }
+}
