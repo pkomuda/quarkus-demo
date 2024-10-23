bd6a83d0375a7f801da26f10d43eff166adf18e0
Przemysław Komuda <p.komuda1@gmail.com>
2024-10-23 20:18:11 +0200

    Add controller

---
+++ src/main/java/com/github/pkomuda/BookController.java
@@ -0,0 +1,34 @@
+package com.github.pkomuda;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Path("/books")
+@ApplicationScoped
+@RequiredArgsConstructor
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class BookController {
+
+    private final BookService bookService;
+
+    @POST
+    public void addBook(BookDto bookDto) {
+        bookService.addBook(bookDto);
+    }
+
+    @GET
+    @Path("/{isbn}")
+    public BookDto getBook(@PathParam("isbn") String isbn) {
+        return bookService.getBook(isbn);
+    }
+
+    @GET
+    public List<BookDto> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+}
