1de67de325cfb887c9ef2717211717a8d6f3d083
Przemysław Komuda <p.komuda1@gmail.com>
2024-10-26 21:59:17 +0200

    Add custom config property

--- src/main/java/com/github/pkomuda/BookService.java
+++ src/main/java/com/github/pkomuda/BookService.java
@@ -3,13 +3,22 @@ package com.github.pkomuda;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import java.util.List;
 
+@Slf4j
 @Transactional
 @ApplicationScoped
 @RequiredArgsConstructor
 class BookService {
+
+    @ConfigProperty(name = "custom.property")
+    String customPropertyInjected;
+    private final String customProperty = ConfigProvider.getConfig().getValue("custom.property", String.class);
+    private static final String customPropertyStatic = ConfigProvider.getConfig().getValue("custom.property", String.class);
 
     private final BookRepository bookRepository;
     private final BookMapper bookMapper;
@@ -25,6 +34,9 @@ class BookService {
     }
 
     public List<BookDto> getAllBooks() {
+        log.info(customPropertyInjected);
+        log.info(customProperty);
+        log.info(customPropertyStatic);
         return bookMapper.toDtos(bookRepository.listAll());
     }
 }
